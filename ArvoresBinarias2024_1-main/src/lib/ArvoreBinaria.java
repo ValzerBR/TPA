7ae81031a35f8e1db7503d0f12478cbfd1da1995
Breno Scalzer <57049102+ValzerBR@users.noreply.github.com>
2024-04-02 08:59:29 -0300

    correção dos comentários das classes da interface, pois na interface havia dois métodos pesquisar

--- ArvoresBinarias2024_1-main/src/lib/ArvoreBinaria.java
+++ ArvoresBinarias2024_1-main/src/lib/ArvoreBinaria.java
@@ -40,49 +40,53 @@ public class ArvoreBinaria<T> implements IArvoreBinaria<T> {
 
         return no;
     }
-
-    public T pesquisar(T valor);
     
-    /**
+   
+    public T pesquisar(T valor, Comparator comparador);
+     /**
      * Método para pesquisar por um elemento na árvore utilizando um comparator passado como parâmetro. Como o comparador a ser usado não é o que 
      indexou a árvore, você deve varrer todos os elementos da arvore na busca. O valor a ser buscado deve ser passado de acordo com o comparador passado.
      * @param valor - será utilizado para passar o valor da chave a ser buscada.
      * @param comparador - Comparator a ser utilizado na busca. 
      * @return caso tenha sido encontrado um elemento com o valor buscado, o mesmo será retornado. Caso contrário retorna null.
      */
-    public T pesquisar(T valor, Comparator comparador);
  
+    public T remover(T valor);
+    
 
     /**
      * Método que busca por um elemento na árvore e, caso encontre, o remove da árvore e o retorna
      * @param valor - será utilizado para passar o valor da chave a ser buscada. Por exemplo, se for um árvore de Alunos indexada por nome, deve-se passar um objeto do tipo aluno com o nome que se deseja buscar.
      * @return caso tenha sido encontrado um elemento com o valor buscado, o elemento será removido da árvore e seu valor (do tipo T) será retornado. Caso contrário retorna null.
      */
-    public T remover(T valor);
+    
+    public int altura();
     
     /**
      * Método que retorna a altura da árvore
      * @return Retorna a altura da árvore. Árvores só com raiz tem altura zero(0). Se raiz for nula retorne -1. 
      */
-    public int altura();
+    
+    public int quantidadeNos();
     
     /**
      * Método que retorna a quantidade de nós da árvore
      * @return Retorna a quantidade de nós da árvore
      */
-    public int quantidadeNos();
 
+    public String caminharEmNivel();
+    
     
     /**
      * Metódo que retona o resultado do caminhamento em nível na árvore.
      * @return String contendo os toString dos valores armazenados nos nós, separados por " \n ". Os nós devem ser percorridos em nível. A String deve iniciar com "[" e finalizar com "]"
      */
-    public String caminharEmNivel();
-    
+   
+    public String caminharEmOrdem();
+     
     /**
      * Metódo que retona o resultado do caminhamento em ordem na árvore.
      * @return String contendo os toString dos valores armazenados nos nós, separados por " \n ". Os nós devem ser percorridos em ordem. A String deve iniciar com "[" e finalizar com "]"
      */
-    public String caminharEmOrdem();
 }
     
