9e149e6efdaa1b8d957399f6c47cc432cae9995e
João Victor Lopes Lopes <20221BSI0439@cefetes.br>
2024-06-04 08:49:49 -0300

    cria aluno para ser usado na etapa 3 no package correto

--- ArvoresBinarias2024_1-main/src/appEtapa3/Aluno.java
+++ ArvoresBinarias2024_1-main/src/appEtapa3/Aluno.java
@@ -1,2 +1,41 @@
-package appEtapa3;public class Aluno {
+package appEtapa3;
+
+import java.util.ArrayList;
+/**
+ *
+ * @author victoriocarvalho
+ *
+ * Essa é a classe Aluno que será utilizada como tipo do conteúdo das árvores nos
+ * programas de teste para redigir os relatórios.
+ */
+
+public class Aluno  {
+    private int matricula;
+    private String nome;
+    private ArrayList<Disciplina> disciplinasCursadas;
+
+    public Aluno(int matricula, String nome){
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public ArrayList<Disciplina> getDisciplinasCursadas(Aluno aluno){
+        return aluno.disciplinasCursadas;
+    }
 }
