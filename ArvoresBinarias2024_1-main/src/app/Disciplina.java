22764ec3a8d5ea05b5c0a45e4ae2a4f9b0e80d2f
João Victor Lopes Lopes <20221BSI0439@cefetes.br>
2024-06-04 08:34:53 -0300

    Adiciona classe disciplina

---
+++ ArvoresBinarias2024_1-main/src/app/Disciplina.java
@@ -0,0 +1,28 @@
+
+package app;
+
+import java.util.ArrayList;
+public class Disciplina {
+    private int codigo;
+    private String nome;
+    private  int cargaHoraria;
+    private ArrayList<Disciplina> preRequisitos;
+    private ArrayList<Aluno> alunos;
+
+    public Disciplina(int codigo, String nome, int cargaHoraria, ArrayList<Disciplina> preRequisitos, ArrayList<Aluno> alunos){
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargaHoraria = cargaHoraria;
+        this.preRequisitos = preRequisitos;
+        this.alunos = alunos;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
